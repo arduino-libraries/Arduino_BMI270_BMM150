1e3bcb95595ab533bc8b5ad24aabd7830ce1a56f
Risto <rotorman@users.noreply.github.com>
2023-02-15 11:25:00 +0100

    LSM9DS1 -> BMI270

--- examples/SimpleGyroscope/SimpleGyroscope.ino
+++ examples/SimpleGyroscope/SimpleGyroscope.ino
@@ -1,12 +1,12 @@
 /*
-  Arduino LSM9DS1 - Simple Gyroscope
+  Arduino BMI270 - Simple Gyroscope
 
-  This example reads the gyroscope values from the LSM9DS1
+  This example reads the gyroscope values from the BMI270
   sensor and continuously prints them to the Serial Monitor
   or Serial Plotter.
 
   The circuit:
-  - Arduino Nano 33 BLE Sense
+  - Arduino Nano 33 BLE Sense Rev2
 
   created 10 Jul 2019
   by Riccardo Rizzo
